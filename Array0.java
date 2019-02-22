a5e98cb17e04ab79700daad0f1f80bc2f0e8fd8f
ravikiran2 <47805907+ravikiran2@users.noreply.github.com>
2019-02-22 15:05:51 +0530

    Add files via upload

---
+++ Array0.java
@@ -0,0 +1,17 @@
+
+public class Array0
+{
+	public static void main(String[] args)
+	{
+		
+int sum[]= {1,0,0,0,1,0,1,1,};
+for(int i=0;i<sum.length;i++)
+
+if(sum[i]==0)
+{
+	System.out.println(sum[i]);
+
+	
+}
+}
+	} 
