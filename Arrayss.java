0037a634173c758f0e036743ecfce3b10557c95d
ravikiran2 <47805907+ravikiran2@users.noreply.github.com>
2019-03-02 19:13:56 +0530

    arrays

---
+++ Arrayss.java
@@ -0,0 +1,21 @@
+
+public class Arrayss 
+{
+	public static void main(String[] args) 
+	{
+	 int [] arr= {65,2,69,5,7,77};
+
+for(int i=0;i<arr.length ;i++)
+{
+	System.out.println(arr[i]);
+	System.out.println(arr.length);
+	System.out.println(arr[i]+2);
+	 
+}
+
+
+
+	}
+
+}
+
