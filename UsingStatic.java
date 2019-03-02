24ecbb600b306ff819f1df9a954be1aa6d26ff44
ravikiran2 <47805907+ravikiran2@users.noreply.github.com>
2019-03-02 19:42:36 +0530

    using static

---
+++ UsingStatic.java
@@ -0,0 +1,17 @@
+
+public class UsingStatic
+{
+int id;
+String name;
+static String college="RRR";
+
+
+  void studentsClass()
+{
+	  System.out.println("your id is "+id);
+	  System.out.println("your name is "+name);
+	  System.out.println("your college is "+college);
+}
+	
+	
+}
---
+++ UsingStaticTest.java
@@ -0,0 +1,20 @@
+
+public class UsingStaticTest {
+
+	public static void main(String[] args) {
+		UsingStatic c = new UsingStatic();
+		UsingStatic c1 = new UsingStatic();
+		UsingStatic c2 = new UsingStatic();
+		c.id = 12;
+		c.name = "ravi";
+		c.studentsClass();
+		c1.id = 20;
+		c1.name = "kiran";
+		c1.studentsClass();
+		c2.id = 46;
+		c2.name = "hhhh";
+		c2.studentsClass();
+
+	}
+
+}
