f2800c64bf55d5d137bbabbdb8620c25f02bdfe1
ravikiran2 <47805907+ravikiran2@users.noreply.github.com>
2019-03-02 19:32:23 +0530

    arrays

---
+++ ArrayDemo.java
@@ -0,0 +1,16 @@
+
+ class ArrayDemo 
+{
+			
+	public static void  display(int x)
+	{
+
+{
+	if(x>=35)
+		System.out.println("pass");
+	else
+		System.out.println("fail");
+}
+
+}
+}
---
+++ ArrayDemoTest.java
@@ -0,0 +1,14 @@
+
+public class ArrayDemoTest 
+{
+
+	public static void main(String[] args) 
+	{
+int x=46;
+ArrayDemo.display(x);
+int y=20;
+ArrayDemo.display(y);
+		
+}
+
+}
